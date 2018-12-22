0ca1724da14584643c71bb74fcc4c8bdc0eab137
May Z <maydeyn@gmail.com>
2018-12-22 01:43:43 -0600

    updated images

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,33 @@
-const path = require("path");
-
 const pokemons = require("../db/data.js");
 
 module.exports = function(app) {
   app.get("/api/pokemons", function(req, res) {
     res.json(pokemons);
   });
+
+  app.post("/api/pokemons", function(req, res) {
+    var userData = req.body;
+    console.log(userData);
+  });
 };
+
+const mysql = require("mysql");
+
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "pokemons_db"
+  });
+}
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err);
+  }
+});
--- server.js
+++ server.js
@@ -12,26 +12,6 @@ app.use(express.json());
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
 
-const mysql = require("mysql");
-var connection;
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-  var connection = mysql.createConnection({
-    host: "localhost",
-    port: 8889,
-    user: "root",
-    password: "root",
-    database: "pokemons_db"
-  });
-}
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err);
-  }
-});
-
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
 });
