c87a5d8450891ee3ba38dd733fea837cc5efeb43
May Z <maydeyn@gmail.com>
2018-12-21 23:56:06 -0600

    updated routing and CSS

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,9 @@
 const path = require("path");
 
-const pokemons = require("../db/data");
+const pokemons = require("../db/data.js");
 
 module.exports = function(app) {
-  app.get("/api/survey", function(req, res) {
+  app.get("/api/pokemons", function(req, res) {
     res.json(pokemons);
   });
 };
